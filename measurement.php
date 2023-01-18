9a93101537c62c2636261cca6c94e76ed1195f95
emmacob123 <88585192+emmacob123@users.noreply.github.com>
2023-01-18 11:17:46 +0100

    measurement page

--- measurement.php
+++ measurement.php
@@ -159,6 +159,94 @@
                                             <div class="orders">
 
                                             </div>
+                                            <form>
+                                                                <div class="form-group row">                                                                    <div class="col-sm-10">
+                                                                        <input type="text" class="form-control" placeholder="Customer Name">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">                                                                    <div class="col-sm-10">
+                                                                        <input type="tel" class="form-control"
+                                                                        placeholder="Phone Number">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                        <div class="col-sm-10">                                                                            <input type="email" class="form-control"
+                                                                            placeholder="Email">
+                                                                        </div>
+                                                                    </div>
+                                                                <div class="form-group row">
+                                                                    <div class="col-sm-10">                                                                        <input type="password" class="form-control"
+                                                                        placeholder="Password input">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                                <div class="col-sm-10">
+                                                                                    <textarea rows="5" cols="5" class="form-control"
+                                                                                    placeholder="Address"></textarea>
+                                                                                </div>
+                                                                            </div>
+                                                                    <div class="form-group row">
+                                                                        <label class="col-sm-2 col-form-label">Select Box</label>
+                                                                        <div class="col-sm-10">
+                                                                            <select name="select" class="form-control">
+                                                                                <option value="opt1">Select One Value Only</option>
+                                                                                <option value="opt2">Type 2</option>
+                                                                                <option value="opt3">Type 3</option>
+                                                                                <option value="opt4">Type 4</option>
+                                                                                <option value="opt5">Type 5</option>
+                                                                                <option value="opt6">Type 6</option>
+                                                                                <option value="opt7">Type 7</option>
+                                                                                <option value="opt8">Type 8</option>
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <label class="col-sm-2 col-form-label">Round Input</label>
+                                                                        <div class="col-sm-10">
+                                                                            <input type="text"
+                                                                            class="form-control form-control-round"
+                                                                            placeholder=".form-control-round">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <label class="col-sm-2 col-form-label">Maximum
+                                                                            Length</label>
+                                                                            <div class="col-sm-10">
+                                                                                <input type="text" class="form-control"
+                                                                                placeholder="Content must be in 6 characters"
+                                                                                maxlength="6">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="form-group row">
+                                                                            <label class="col-sm-2 col-form-label">Disable
+                                                                                Autocomplete</label>
+                                                                                <div class="col-sm-10">
+                                                                                    <input type="text" class="form-control"
+                                                                                    placeholder="Autocomplete Off"
+                                                                                    autocomplete="off">
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-group row">
+                                                                                <label class="col-sm-2 col-form-label">Static Text</label>
+                                                                                <div class="col-sm-10">
+                                                                                    <div class="form-control-static">Hello !... This is
+                                                                                        static text
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-group row">
+                                                                                <label class="col-sm-2 col-form-label">Color</label>
+                                                                                <div class="col-sm-10">
+                                                                                    <input type="color" class="form-control">
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-group row">
+                                                                                <label class="col-sm-2 col-form-label">Upload File</label>
+                                                                                <div class="col-sm-10">
+                                                                                    <input type="file" class="form-control">
+                                                                                </div>
+                                                                            </div>
+                                                                        </form>
                                             <table class="table">
                                                 <thead>
                                                     <th>Id</th>
